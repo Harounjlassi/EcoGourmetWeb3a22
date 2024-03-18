79fe119bfa08b4091c1223d483f5a2d217c3093b
Harounjlassi <jlassi.haroun@esprit.tn>
2024-03-18 03:25:03 +0100

    firstIntegration

---
+++ src/Controller/TrController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrController extends AbstractController
+{
+    #[Route('/tr', name: 'app_tr')]
+    public function index(): Response
+    {
+        return $this->render('tr/index.html.twig', [
+            'controller_name' => 'TrController',
+        ]);
+    }
+}
